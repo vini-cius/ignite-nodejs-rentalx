5af826207b090e32543578019f714251ea283711
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2022-01-01 22:26:18 -0300

    Validando o cadastro de categoria

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository()
+
+categoriesRoutes.get('/', (request, response) => {
+	const categories = categoriesRepository.list();
+
+	return response.json(categories);
+});
+
+categoriesRoutes.post('/', (request, response) => {
+	const { name, description } = request.body;
+
+	const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+	if (categoryAlreadyExists) {
+		return response.status(400).json({ error: 'Category already exists' });
+	}
+
+	categoriesRepository.create({ name, description });
+
+	return response.status(201).json({ message: 'Categoria criada com sucesso!' });
+});
+
+export { categoriesRoutes };
