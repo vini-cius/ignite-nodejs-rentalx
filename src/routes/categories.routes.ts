48300b11abcc1ae4d5dcc3e5344ff0d8cc4c87bc
Vinicius de Souza Santos <vinicius.santos@aztronic.com.br>
2022-02-23 23:13:37 -0300

    Corrigindo as importações

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
