c9a39788ca3e10fea8495d16908466ecd89b3fa8
Vinicius de Souza Santos <vinicius.santos@aztronic.com.br>
2022-03-21 23:11:28 -0300

    specification routes

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,13 @@
+import { Specification } from "../model/Specification";
+
+interface ICreateSpecificatioDTO {
+	name: string;
+	description: string;
+}
+
+interface ISpecificationsRepository {
+	create({ description, name }: ICreateSpecificatioDTO): void;
+	findByName(name: string): Specification;
+}
+
+export { ICreateSpecificatioDTO, ISpecificationsRepository };
--- src/services/CreateSpecificationService.ts
+++ src/services/CreateSpecificationService.ts
@@ -1,5 +1,22 @@
+import { ISpecificationsRepository } from "../modules/cars/repositories/ISpecificationsRepository";
+
+interface IRequest {
+	name: string;
+	description: string;
+}
+
 class CreateSpecificationService {
-	execute() { }
+	constructor(private specificationsRepository: ISpecificationsRepository) { }
+
+	execute({ name, description }: IRequest): void {
+		const specificationAlredyExists = this.specificationsRepository.findByName(name);
+
+		if (specificationAlredyExists) {
+			throw new Error('Specification alredy exists!');
+		}
+
+		this.specificationsRepository.create({ name, description });
+	}
 }
 
 export { CreateSpecificationService };
