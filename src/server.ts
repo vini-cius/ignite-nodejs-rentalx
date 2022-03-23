5a56da5d0753dcfe6431ddb41d403cc08bedbc5f
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2022-03-22 21:47:26 -0300

    Criando a documentação de criação de categoria

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import { router } from './routes';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(router)
 
 app.listen(3333, () => {
